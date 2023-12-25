aec0f285ef5c88aaa98b3bd75330ad12d7df0a6b
Your Name <dunghuynh110496@gmail.com>
2023-12-26 02:03:05 +1100

    add basic add e speaking

--- src/private_members.js
+++ src/private_members.js
@@ -1,4 +1,6 @@
 export const private_members = [
   "dunghuynh110496@gmail.com",
-  "minhngoan091996@gmail.com"
+  "minhngoan091996@gmail.com",
+  "minhngoan020996@gmail.com",
+  "minhngoan296@gmail.com"
 ]
